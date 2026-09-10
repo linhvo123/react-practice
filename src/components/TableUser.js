ddee69681363a6f99ba68af058f7f731375dfe3c
linhvo123 <vodinhlinh2058@gmail.com>
2026-09-10 14:28:31 +0700

    UI/Header

---
+++ src/components/TableUser.js
@@ -0,0 +1,22 @@
+const TableUser = () => {
+  return (
+    <div className="table-container">
+      <table>
+        <thead>
+          <tr>
+            <th>Name</th>
+            <th>Email</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr>
+            <td>John Doe</td>
+            <td>john@example.com</td>
+          </tr>
+        </tbody>
+      </table>
+    </div>
+  );
+};
+
+export default TableUser;
